2ee6fc699c9237b285cf3edc05fb536cc294931b
Tim Rupp <caphrim007@gmail.com>
2017-12-06 12:40:52 -0800

    update irule

--- labs/lab3.10/files/irule-01.tcl
+++ labs/lab3.10/files/irule-01.tcl
@@ -10,7 +10,6 @@ when RULE_INIT {
     }
 
     when HTTP_REQUEST {
-       set apmsessionid [HTTP::cookie value MRHSession]
        set persist_cookie [HTTP::cookie value $static::ckname]
        set clientless_mode 0
        set form_mode 0
@@ -38,44 +37,6 @@ when RULE_INIT {
              }
           }
        }
-       if { $clientless_mode || $form_mode } {
-          if { [HTTP::cookie exists "MRHSession"] } {set apmstatus [ACCESS::session exists -state_allow $apmsessionid]} else {set apmstatus 0}
-          if { !($apmstatus) && [HTTP::cookie exists $static::ckname] } {set apmpersiststatus [ACCESS::session exists -state_allow $persist_cookie]} else {set apmpersiststatus 0}
-          if { ($apmpersiststatus) && !($apmstatus) } {
-             # Add MRHSession cookie for non browser user-agent first request and persistent cookie present
-             if { [catch {HTTP::cookie insert name "MRHSession" value $persist_cookie} ] } {log local0. "[IP::client_addr]:[TCP::client_port] : TCL error on HTTP cookie insert MRHSession : URL : [HTTP::host][HTTP::path] - Headers : [HTTP::request]"} else {return}
-          }
-       } else { return }
-       if { $clientless_mode && !($apmstatus)} {
-          if { !([HTTP::header Authorization] == "") } {
-             set clientless(insert_mode) 1
-             set clientless(username)    [ string tolower [HTTP::username] ]
-             set clientless(password)    [HTTP::password]
-             binary scan [md5 "$clientless(password)"] H* clientless(hash)
-             set user_key "$clientless(username).$clientless(hash)"
-             set clientless(cookie_list)             [ ACCESS::user getsid $user_key ]
-             if { [ llength $clientless(cookie_list) ] != 0 } {
-                set clientless(cookie) [ ACCESS::user getkey [ lindex $clientless(cookie_list) 0 ] ]
-                if { $clientless(cookie) != "" } {
-                   HTTP::cookie insert name MRHSession value $clientless(cookie)
-                   set clientless(insert_mode) 0
-                }
-             }
-             if { $clientless(insert_mode) } {
-                HTTP::header insert "clientless-mode" 1
-                HTTP::header insert "username" $clientless(username)
-                HTTP::header insert "password" $clientless(password)
-            }
-            unset clientless
-          } else {HTTP::header insert "clientless-mode" 1}
-       } elseif {$form_mode && !($apmstatus) && !([HTTP::path] equals $static::FormBaseURL)}{
-          HTTP::respond 403 -version "1.1" noserver \
-             $static::HeadAuthReq "https://[HTTP::host]$static::FormBaseURL" \
-             $static::HeadAuthRet "https://[HTTP::host]$static::FormReturnURL" \
-             $static::HeadAuthSize $static::HeadAuthSizeVal \
-             "Connection" "Close"
-          return
-       }
     }
 
     when HTTP_RESPONSE {
@@ -90,53 +51,5 @@ when RULE_INIT {
        if { ([info exists "apmpersiststatus"]) && ($apmpersiststatus) } {
              HTTP::cookie insert name MRHSession value $persist_cookie path "/"
              HTTP::cookie secure MRHSession enable
-       }
-    }
-
-    when ACCESS_SESSION_STARTED {
-       if {([info exists "clientless_mode"])} {
-          ACCESS::session data set session.clientless $clientless_mode
-       }
-       if { [ info exists user_key ] } {
-          ACCESS::session data set "session.user.uuid" $user_key
-       }
-    }
-
-    when ACCESS_POLICY_COMPLETED {
-       if { ([info exists "clientless_mode"]) && ($clientless_mode) && ([ACCESS::policy result] equals "deny") } {
-          ACCESS::respond 401 noserver WWW-Authenticate "Basic realm=$static::Basic_Realm_Text" Connection close
-          ACCESS::session remove
-       }
-    }
-
-    when ACCESS_ACL_ALLOWED {
-       switch -glob [string tolower [HTTP::path]] {
-          "/sp-ofba-form" {
-             ACCESS::respond 302 noserver Location "https://[HTTP::host]$static::FormReturnURL"
-          }
-          "/sp-ofba-completed" {
-             ACCESS::respond 200 content {
-                <html>
-                   <head><title>Authenticated</title></head>
-                   <body><b>Good Work, you are Authenticated</b></body>
-                </html>
-             } noserver
-          }
-          "*/signout.aspx" {
-             # Disconnect session and redirect to APM logout Page
-             ACCESS::respond 302 noserver Location "/vdesk/hangup.php3"
-             return
-          }
-          "/_layouts/accessdenied.aspx" {
-             # Disconnect session and redirect to APM Logon Page
-             if {[string tolower [URI::query [HTTP::uri] loginasanotheruser]] equals "true" } {
-                ACCESS::session remove
-                ACCESS::respond 302 noserver Location "/"
-                return
-             }
-          }
-          default {
-             # No Actions
-          }
        }
     }
