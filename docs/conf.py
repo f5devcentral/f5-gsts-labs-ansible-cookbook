5a9084816791218a0b6c255e710e69dc27b65225
vtog <v.tognaci@f5.com>
2018-09-27 16:51:46 -0500

    updated index and class README

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,7 @@
 # ------------
 #
 # REQUIRED: Your class/lab name
-classname = "F5 Ansible Cookbook"
+classname = "F5 Labs for Ansible Cookbooks"
 
 # OPTIONAL: The URL to the GitHub Repository for this class
 github_repo = "https://github.com/f5devcentral/f5-gsts-labs-ansible-cookbook"
